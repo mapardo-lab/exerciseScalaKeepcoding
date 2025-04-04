663a0ad86f898558bba1d22d24735438e3efa972
Miguel Angel Pardo <mapardo@gmail.com>
2025-04-04 21:52:50 +0200

    Ejercicio 1 terminado

--- src/main/scala/examen/Examen.scala
+++ src/main/scala/examen/Examen.scala
@@ -1,12 +1,46 @@
 package examen
 
 import org.apache.spark.rdd._
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
 
 object Examen {
 
   /**
+   * Filtra los estudiantes con una calificación mayor que 8
+   *
+   * @param estudiantes contiene nombre, edad y calificación
+   * @return
+   */
+    // TODO convertir el dataframe a tupla y devolverlo para luego incluirlo en el test should
+  def ejercicio1a(estudiantes: DataFrame): DataFrame = estudiantes.filter(col("calificacion") > 8)
+
+  /**
+   * Devolver los nombres ordenados por orden ascendente de calificación
+   *
+   * @param estudiantes contine nombre, edad y calificación
+   * @return
+   */
+  // TODO convertir el dataframe a tupla y devolverlo para luego incluirlo en el test should
+  def ejercicio1b(estudiantes: DataFrame): DataFrame = estudiantes
+    .sort(desc("calificacion"))
+    .select("nombre")
+
+
+  /**Ejercicio 2: UDF (User Defined Function)
+   Pregunta: Define una función que determine si un número es par o impar.
+   Aplica esta función a una columna de un DataFrame que contenga una lista de números.
+   */
+  def ejercicio2(numeros: DataFrame): DataFrame =  {
+    =
+  }
+
+
+
+
+  /**
    * Cuenta el número de ocurrencias por palabra
-   * 
+   *
    * @param rdd Conjuntos de palabras
    * @return Tupla con la palabra y el número de veces que aparece
    */
--- src/test/scala/examen/ExamenTest.scala
+++ src/test/scala/examen/ExamenTest.scala
@@ -1,11 +1,81 @@
 package examen
 
+import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.sql.types._
 import examen.Examen._
 import utils.TestInit
 
-class ExamenTest extends TestInit{
+class ExamenTest extends TestInit {
 
   val sc = spark.sparkContext
+
+  val schemaEstudiantes = StructType(Seq(
+    StructField("nombre", StringType, nullable = false),
+    StructField("edad", IntegerType, nullable = false),
+    StructField("calificacion", DoubleType, nullable = false)
+  ))
+
+  val tuplasEstudiantes: Seq[(String, Int, Double)] =
+    Seq(("Juan", 35, 3),
+      ("Pepe", 23, 8),
+      ("Lucas", 27, 8.5),
+      ("Paco", 50, 4),
+      ("Miguel", 45, 3.2),
+      ("Ruben", 33, 5.8),
+      ("Sofia", 27, 8.8),
+      ("David", 37, 9),
+    )
+
+  "ejercicio1" should "crear y mortrar dataframe" in {
+
+    val dfEstudiantes: DataFrame = newDf(tuplasEstudiantes.map(Row.fromTuple), schemaEstudiantes)
+
+    dfEstudiantes.printSchema()
+
+    dfEstudiantes.show()
+  }
+
+  "ejercicio1" should "filtrar por calificación" in {
+
+    val dfEstudiantes: DataFrame = newDf(tuplasEstudiantes.map(Row.fromTuple), schemaEstudiantes)
+
+    val dfOut: DataFrame = ejercicio1a(dfEstudiantes)
+
+    dfOut.show()
+  }
+
+  "ejercicio1" should "ordenar nombres según calificación" in {
+
+    val dfEstudiantes: DataFrame = newDf(tuplasEstudiantes.map(Row.fromTuple), schemaEstudiantes)
+
+    val dfOut: DataFrame = ejercicio1b(dfEstudiantes)
+
+    dfOut.show()
+  }
+
+  "ejercicio2" should "reconocer números negativos" in {
+    val schemaTemperaturas = StructType(Seq(
+      StructField("ciudad", StringType, nullable = false),
+      StructField("temperaturaMinima", DoubleType, nullable = false)
+    ))
+
+    val tuplasTemperaturas: Seq[(String, Double)] =
+      Seq(("Pekin", 12.1),
+        ("Madrid", 5.0),
+        ("Estocolmo", -10.3),
+        ("Barcelona", 27.6),
+        ("Oslo", -5.2),
+        ("Paris", 0.0)
+      )
+
+    val dfTemperaturas: DataFrame = newDf(tuplasTemperaturas.map(Row.fromTuple), schemaTemperaturas)
+
+    val dfOut: DataFrame = ejercicio2(dfTemperaturas)
+
+    dfOut.show()
+
+
+  }
 
   "ejercicio4" should "contar número ocurrencias por palabra" in {
 
